e8c7632bf81ae3ac4fb989cd74c7963e06ca1f28
Stefanos Touf <stefetoufe@gmail.com>
2021-09-20 18:06:16 +0300

    Feature: use table type

--- src/main/scala/Common.scala
+++ src/main/scala/Common.scala
@@ -1,24 +1,32 @@
 import doobie.util.fragment.Fragment
+import doobie.implicits._
 
-case class FieldValue(field: Field[Any], value: Fragment)
+object Common:
 
-object Common {
+  case class Field[+B](name: Fragment)
+
+  case class FieldValue(field: Field[Any], value: Fragment)
+
+  case class PrimaryKey(field: Field[Any])
+  case class ForeignKey(field: Field[Any], references: Model[?, ?])
+
+  case class Table(private val nameStr: String):
+    val name = sql"$nameStr"
+
   import cats.Monoid
   import cats.implicits._
 
   trait Fields
 
-  trait ModelMeta[B] {
-    val table: Fragment
+  trait ModelMeta[B]:
+    val pk: PrimaryKey
+    val fk: Option[ForeignKey] = None
+    val table: Table
     def mapper(entity: B): List[FieldValue]
-  }
 
-  case class Model[A, B <: Fields](fields: B, meta: ModelMeta[A])
+  case class Model[A, +B <: Fields](fields: B, meta: ModelMeta[A])
 
-  trait Completable(
-      val contents: List[Fragment]
-  ) {
+  trait Completable(val contents: List[Fragment]):
     def complete: Fragment = contents.fold(Monoid[Fragment].empty)(_ combine _)
 
-  }
-}
+end Common
