f239c7687c93beb73a5119bdbf3c275e46ff4bfe
Stefanos Touf <stefetoufe@gmail.com>
2021-09-30 17:20:41 +0300

    Chore: table name on derive meta

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -8,32 +8,32 @@ object Models:
 
   case class Photo(name: String, photographer: String)
 
-  object Photo extends Model[Photo]("photo"):
+  object Photo extends Model[Photo]:
     val name         = column[String]("name")
     val photographer = column[String]("photographer_name")
 
     object Meta:
-      given ModelMeta[Photo] = deriveMeta(name, photographer)
+      given ModelMeta[Photo] = deriveMeta("photo", name, photographer)
 
   given Relation[Photo, Person](Photo.photographer)
 
   case class Person(name: String, age: Int, nickname: String)
 
-  object Person extends Model[Person]("person"):
+  object Person extends Model[Person]:
     val name     = column[String]("name")
     val age      = column[Int]("age")
     val nickname = column[String]("nickname")
 
     object Meta:
-      given ModelMeta[Person] = deriveMeta(name, age, nickname)
+      given ModelMeta[Person] = deriveMeta("person", name, age, nickname)
 
   case class Pet(name: String, owner: String)
 
-  object Pet extends Model[Pet]("pet"):
+  object Pet extends Model[Pet]:
     val name  = column[String]("name")
     val owner = column[String]("owner_name")
 
     object Meta:
-      given ModelMeta[Pet] = deriveMeta(name, owner)
+      given ModelMeta[Pet] = deriveMeta("pet", name, owner)
 
   given Relation[Pet, Person](Pet.owner)
